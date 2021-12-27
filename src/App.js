30e76e1fbc8b57407538b26dc290420d0f7e5f00
LizaUrbas <92731788+LizaUrbas@users.noreply.github.com>
2021-12-27 12:37:32 +0300

    CreateReactApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
+
 import './App.css';
 
+const Header = () => {
+  return(
+    <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1>Processing</h1>
+  </header>
+
+  );
+}
+
+const Main = ({title,price}) =>{
+  
+
+  return(
+    <main>
+      <h2>
+        {title}
+      </h2>
+      <h3>
+        {price}
+      </h3>
+     
+    </main>
+  );
+}
+
+const Footer = () =>{
+  return(
+    <footer>
+      <a>link something</a>
+    </footer>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
+     <div>
+       <p>span</p>
+     </div>
+     <Main title="PC" price={4500}/>
+     <Main title="Laptop" price={5000}/>
+     <Main title="Phone" price={2000}/>
+     <Footer/>
     </div>
   );
 }
